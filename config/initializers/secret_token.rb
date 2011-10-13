1f5d70d83bf7ca714b5131a9ca0911b2538b2296
Erin Walker <ewalker@ewalkerblog.co.za>
2011-10-13 16:23:46 +0200

    'first_commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RugbyList::Application.config.secret_token = 'bee090f957f26563efc0ae82af673f234b7a2c64322b8490064949ae55f9430400912ede0b754b521ffb60b8d881893fc4a32e102954669a16469f3651c7f214'
